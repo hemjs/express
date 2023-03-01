165873162e4c0fcdb98ea81b01b73fc425fb3e7b
augusito <kaugustus15@gmail.com>
2023-03-01 19:26:01 +0300

    fix: missing dependency

--- tests/express-adapter.test.ts
+++ tests/express-adapter.test.ts
@@ -1,6 +1,7 @@
+import * as express from 'express';
 import { ExpressAdapter } from '../src';
 
 test('should return an instance of ExpressAdapter', () => {
-  const adapter = new ExpressAdapter();
+  const adapter = new ExpressAdapter(express());
   expect(adapter).toBeInstanceOf(ExpressAdapter);
 });
