de9145e08ca6e1f4b538b3ca0fae8bc839232183
augusito <kaugustus15@gmail.com>
2023-03-02 10:13:44 +0300

    refactor: test description

--- tests/integration/adapter.instance.test.ts
+++ tests/integration/adapter.instance.test.ts
@@ -2,7 +2,7 @@ import * as express from 'express';
 import * as request from 'supertest';
 import { ExpressAdapter } from '../../src';
 
-describe('Express Application', () => {
+describe('.getInstance()', () => {
   let instance: express.Application;
 
   beforeEach(() => {
@@ -10,7 +10,7 @@ describe('Express Application', () => {
     instance = adapter.getInstance();
   });
 
-  it('should support express application', async () => {
+  it('should support express application instance', async () => {
     instance.get('/', (req: any, res: any) => res.send('value'));
     await request(instance).get('/').expect(200, 'value');
   });
