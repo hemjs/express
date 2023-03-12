34beea04291f6cc1d9044e9bd4c5737d0acd4a15
augusito <kaugustus15@gmail.com>
2023-03-12 10:24:14 +0300

    refactor: next function to any

--- src/types.ts
+++ src/types.ts
@@ -2,28 +2,26 @@ import { Type } from '@hemtypes/core';
 
 export type PathArgument = string | RegExp | (string | RegExp)[];
 
-export type NextFunction = (err?: any) => void;
-
 export type ErrorHandler<TRequest = any, TResponse = any> = (
   err: any,
   req: TRequest,
   res: TResponse,
-  next?: NextFunction,
+  next?: any,
 ) => any;
 
 export type RequestHandler<TRequest = any, TResponse = any> = (
   req: TRequest,
   res: TResponse,
-  next?: NextFunction,
+  next?: any,
 ) => any;
 
 export interface HemHandler<TRequest = any, TResponse = any> {
-  handle(req: TRequest, res: TResponse, next?: Function): any;
+  handle(req: TRequest, res: TResponse, next?: any): any;
 }
 
 export interface HemMiddleware<TRequest = any, TResponse = any> {
-  process(err: any, req: TRequest, res: TResponse, next?: NextFunction): any;
-  process(req: TRequest, res: TResponse, next?: NextFunction): any;
+  process(req: TRequest, res: TResponse, next?: any): any;
+  process(err: any, req: TRequest, res: TResponse, next?: any): any;
 }
 
 export type Handler =
